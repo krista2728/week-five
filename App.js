532889311aab131d9ca85d652ce8801ad013b5c1
krista2728 <121305968+krista2728@users.noreply.github.com>
2023-03-11 13:27:58 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import { data } from './data';
+import './App.css';
+
+function App() {
+
+const [person, setPerson] = useState(0);
+const {id, name, description, age, image} = data[person];
+
+
+const previousPerson = () => {
+  setPerson((person => {
+    person --;
+    if (person < 0) {
+      return data.length-1;
+    }
+    return person;
+  }))
+
+}
+
+const nextPerson = () => {
+  setPerson((person => {
+    person ++;
+    if (person > data.length -1) {
+      person = 0;
+    }
+    return person;
+  }))
+}
+
+  return (<div>
+
+<div className='container'>
+  <img src={image} width="300px" alt='person'/>
+  </div>
+
+
+  <div className='container'>
+  <h1> {id} - {name}</h1>
+  </div>
+
+  <div className='container'>
+<h2> {description} </h2>
+</div>
+
+<div className='container'>
+<h2> {age} years old</h2>
+</div>
+
+<div className='btn container'>
+<button onClick={previousPerson}>Previous</button>
+<button onClick={nextPerson}>Next</button>
+</div>
+
+    </div>
+  );
+}
+
+export default App;
